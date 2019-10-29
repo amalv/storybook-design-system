fcc942032ef9cb3958dd7d79f7b757d7fcf9ecce
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-10-29 18:37:56 +0100

    Add a11y addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 import { GlobalStyle } from '../src/shared/global';
 
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
